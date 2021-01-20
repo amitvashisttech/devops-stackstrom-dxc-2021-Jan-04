91492624c2f48cc2bc92d13b66c084d874403587
Amit Vashist <amitvashist7@outlook.com>
2021-01-20 11:55:25 +0000

    Providers

---
+++ 08-Terraform/03-Resources/resources.tf
@@ -0,0 +1,38 @@
+# Configure a Cloud Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "frontend" {
+# AMZ Linux
+   ami   = "ami-0be2609ba883822ec"
+# Ubuntu - 16.04
+   #ami   = "ami-0739f8cdb239fe9ae"
+   instance_type = "t2.micro"
+   depends_on = [aws_instance.backend]
+   count = 1
+   tags = {
+    Name = "Frontend"
+   }
+   
+   lifecycle { 
+     create_before_destroy = true
+   }
+}
+
+
+
+
+resource "aws_instance" "backend" {
+   ami   = "ami-0be2609ba883822ec"
+   instance_type = "t2.micro"
+   count = 1
+   tags = {
+    Name = "backend"
+   }
+   
+   lifecycle { 
+     prevent_destroy = false
+   }
+}
